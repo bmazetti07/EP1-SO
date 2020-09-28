6faec28ca11c22550840906d3d241794d8dd6e64
Willian Hiroshi <willhiroshi@hotmail.com>
2020-09-28 12:27:32 -0300

    Começando LiveShare

--- ep1.c
+++ ep1.c
@@ -9,43 +9,43 @@ char processos[10000][30];
 
 void * work (void * arguments) {
     int index = *( (int *) arguments);
-  	// printf ("index = %d\n", index);
-  	float time = dts[index];
+    // printf ("index = %d\n", index);
+    float time = dts[index];
     int aux;
-	
-  	while (time > 0) {
-  		aux = 10000 * 10000;
+    
+    while (time > 0) {
+        aux = 10000 * 10000;
         aux ++;
-    	time = time - 0.1;
-   	   	usleep (100000);
+        time = time - 0.1;
+        usleep (100000);
     }
-  	
-  	return NULL;
+      
+    return NULL;
 }
 
 void readFile (char * filename) {
-	FILE * trace;
-  	trace = fopen (filename, "r");
-  	         
-    while (!feof (trace)){
+    FILE * trace;
+    trace = fopen (filename, "r");
+               
+    while (!feof (trace)) {
         if (fscanf (trace, "%s %d %d %d", processos[nProc], &starts[nProc], &dts[nProc], &deadlines[nProc]) != 4)
-        	continue;
+            continue;
         printf ("%s %d %d %d\n", processos[nProc], starts[nProc], dts[nProc], deadlines[nProc]);
-      	tempos[starts[nProc]] ++;
+        tempos[starts[nProc]] ++;
         nProc ++;
- 	}
+    }
 
     fclose (trace);
 }
 
-void fsfc () {
-  	for (int i = 0; i < nProc; i ++) {
-    	pthread_t thread;
+void fcfs () {
+    for (int i = 0; i < nProc; i ++) {
+        pthread_t thread;
         int thread_args[nProc];
         thread_args[i] = i;
-      
+        
         if (pthread_create (&thread, NULL, work, &thread_args[i])) {
-        	printf ("\n ERRO na criação da thread!");
+            printf ("\n ERRO na criação da thread!");
             exit (1);
         }
 
@@ -56,13 +56,24 @@ void fsfc () {
     }
 }
 
+void srtn () {
+    
+
+
+
+}
+
 int main (int argc, char **argv) {
- 	int type = atoi (argv[1]);
-  	readFile (argv[2]);
+    int type = atoi (argv[1]);
+    readFile (argv[2]);
   
     if (type == 1) {
-    	fsfc ();
+        fcfs ();
+    }
+
+    else if (type == 2) {
+        srtn ();
     }
   
-  	return 0;
+    return 0;
 }
